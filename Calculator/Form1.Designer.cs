f01672aece951310c18b7d8e4861887db2118d67
Teooo28 <andra-teodora.aghirasimoae@s.unibuc.ro>
2026-06-03 13:21:35 +0300

    Initial commit Calculator

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,392 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bSageata = new System.Windows.Forms.Button();
+            this.bC = new System.Windows.Forms.Button();
+            this.bCE = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.bRadical = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.bDiv = new System.Windows.Forms.Button();
+            this.bMod = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.bOri = new System.Windows.Forms.Button();
+            this.bInvers = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.bMinus = new System.Windows.Forms.Button();
+            this.bEgal = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bPunct = new System.Windows.Forms.Button();
+            this.bPlus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 82);
+            this.textBox1.TabIndex = 0;
+            // 
+            // bSageata
+            // 
+            this.bSageata.BackColor = System.Drawing.Color.Azure;
+            this.bSageata.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bSageata.Location = new System.Drawing.Point(13, 114);
+            this.bSageata.Name = "bSageata";
+            this.bSageata.Size = new System.Drawing.Size(45, 39);
+            this.bSageata.TabIndex = 1;
+            this.bSageata.Text = "←";
+            this.bSageata.UseVisualStyleBackColor = false;
+            this.bSageata.Click += new System.EventHandler(this.bSageata_Click);
+            // 
+            // bC
+            // 
+            this.bC.BackColor = System.Drawing.Color.Azure;
+            this.bC.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bC.Location = new System.Drawing.Point(64, 114);
+            this.bC.Name = "bC";
+            this.bC.Size = new System.Drawing.Size(45, 39);
+            this.bC.TabIndex = 2;
+            this.bC.Text = "C";
+            this.bC.UseVisualStyleBackColor = false;
+            this.bC.Click += new System.EventHandler(this.bC_Click);
+            // 
+            // bCE
+            // 
+            this.bCE.BackColor = System.Drawing.Color.Azure;
+            this.bCE.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bCE.Location = new System.Drawing.Point(115, 114);
+            this.bCE.Name = "bCE";
+            this.bCE.Size = new System.Drawing.Size(45, 39);
+            this.bCE.TabIndex = 3;
+            this.bCE.Text = "CE";
+            this.bCE.UseVisualStyleBackColor = false;
+            this.bCE.Click += new System.EventHandler(this.bCE_Click);
+            // 
+            // b7
+            // 
+            this.b7.BackColor = System.Drawing.Color.Azure;
+            this.b7.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b7.Location = new System.Drawing.Point(13, 159);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(45, 39);
+            this.b7.TabIndex = 4;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = false;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // bRadical
+            // 
+            this.bRadical.BackColor = System.Drawing.Color.Azure;
+            this.bRadical.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bRadical.Location = new System.Drawing.Point(167, 114);
+            this.bRadical.Name = "bRadical";
+            this.bRadical.Size = new System.Drawing.Size(45, 39);
+            this.bRadical.TabIndex = 5;
+            this.bRadical.Text = "√";
+            this.bRadical.UseVisualStyleBackColor = false;
+            this.bRadical.Click += new System.EventHandler(this.bRadical_Click);
+            // 
+            // b8
+            // 
+            this.b8.BackColor = System.Drawing.Color.Azure;
+            this.b8.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b8.Location = new System.Drawing.Point(64, 159);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(45, 39);
+            this.b8.TabIndex = 6;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = false;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.BackColor = System.Drawing.Color.Azure;
+            this.b9.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b9.Location = new System.Drawing.Point(115, 159);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(45, 39);
+            this.b9.TabIndex = 7;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = false;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // bDiv
+            // 
+            this.bDiv.BackColor = System.Drawing.Color.Azure;
+            this.bDiv.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bDiv.Location = new System.Drawing.Point(167, 159);
+            this.bDiv.Name = "bDiv";
+            this.bDiv.Size = new System.Drawing.Size(45, 39);
+            this.bDiv.TabIndex = 8;
+            this.bDiv.Text = "/";
+            this.bDiv.UseVisualStyleBackColor = false;
+            this.bDiv.Click += new System.EventHandler(this.bDiv_Click);
+            // 
+            // bMod
+            // 
+            this.bMod.BackColor = System.Drawing.Color.Azure;
+            this.bMod.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bMod.Location = new System.Drawing.Point(218, 159);
+            this.bMod.Name = "bMod";
+            this.bMod.Size = new System.Drawing.Size(45, 39);
+            this.bMod.TabIndex = 9;
+            this.bMod.Text = "%";
+            this.bMod.UseVisualStyleBackColor = false;
+            this.bMod.Click += new System.EventHandler(this.bMod_Click);
+            // 
+            // b4
+            // 
+            this.b4.BackColor = System.Drawing.Color.Azure;
+            this.b4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b4.Location = new System.Drawing.Point(13, 204);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(45, 39);
+            this.b4.TabIndex = 10;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = false;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.BackColor = System.Drawing.Color.Azure;
+            this.b5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b5.Location = new System.Drawing.Point(64, 204);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(45, 39);
+            this.b5.TabIndex = 11;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = false;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.BackColor = System.Drawing.Color.Azure;
+            this.b6.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b6.Location = new System.Drawing.Point(115, 204);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(45, 39);
+            this.b6.TabIndex = 12;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = false;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // bOri
+            // 
+            this.bOri.BackColor = System.Drawing.Color.Azure;
+            this.bOri.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bOri.Location = new System.Drawing.Point(167, 204);
+            this.bOri.Name = "bOri";
+            this.bOri.Size = new System.Drawing.Size(45, 39);
+            this.bOri.TabIndex = 13;
+            this.bOri.Text = "*";
+            this.bOri.UseVisualStyleBackColor = false;
+            this.bOri.Click += new System.EventHandler(this.bOri_Click);
+            // 
+            // bInvers
+            // 
+            this.bInvers.BackColor = System.Drawing.Color.Azure;
+            this.bInvers.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bInvers.Location = new System.Drawing.Point(218, 204);
+            this.bInvers.Name = "bInvers";
+            this.bInvers.Size = new System.Drawing.Size(45, 39);
+            this.bInvers.TabIndex = 14;
+            this.bInvers.Text = "1/x";
+            this.bInvers.UseVisualStyleBackColor = false;
+            this.bInvers.Click += new System.EventHandler(this.bInvers_Click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.Azure;
+            this.b1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b1.Location = new System.Drawing.Point(13, 249);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(45, 39);
+            this.b1.TabIndex = 15;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.Azure;
+            this.b2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b2.Location = new System.Drawing.Point(64, 249);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(45, 39);
+            this.b2.TabIndex = 16;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.Azure;
+            this.b3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b3.Location = new System.Drawing.Point(115, 249);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(45, 39);
+            this.b3.TabIndex = 17;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // bMinus
+            // 
+            this.bMinus.BackColor = System.Drawing.Color.Azure;
+            this.bMinus.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bMinus.Location = new System.Drawing.Point(167, 249);
+            this.bMinus.Name = "bMinus";
+            this.bMinus.Size = new System.Drawing.Size(45, 39);
+            this.bMinus.TabIndex = 18;
+            this.bMinus.Text = "-";
+            this.bMinus.UseVisualStyleBackColor = false;
+            this.bMinus.Click += new System.EventHandler(this.bMinus_Click);
+            // 
+            // bEgal
+            // 
+            this.bEgal.BackColor = System.Drawing.Color.Azure;
+            this.bEgal.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bEgal.Location = new System.Drawing.Point(218, 249);
+            this.bEgal.Name = "bEgal";
+            this.bEgal.Size = new System.Drawing.Size(45, 84);
+            this.bEgal.TabIndex = 19;
+            this.bEgal.Text = "=";
+            this.bEgal.UseVisualStyleBackColor = false;
+            this.bEgal.Click += new System.EventHandler(this.bEgal_Click);
+            // 
+            // b0
+            // 
+            this.b0.BackColor = System.Drawing.Color.Azure;
+            this.b0.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b0.Location = new System.Drawing.Point(13, 294);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(96, 39);
+            this.b0.TabIndex = 20;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = false;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // bPunct
+            // 
+            this.bPunct.BackColor = System.Drawing.Color.Azure;
+            this.bPunct.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bPunct.Location = new System.Drawing.Point(115, 294);
+            this.bPunct.Name = "bPunct";
+            this.bPunct.Size = new System.Drawing.Size(45, 39);
+            this.bPunct.TabIndex = 21;
+            this.bPunct.Text = ",";
+            this.bPunct.UseVisualStyleBackColor = false;
+            this.bPunct.Click += new System.EventHandler(this.bPunct_Click);
+            // 
+            // bPlus
+            // 
+            this.bPlus.BackColor = System.Drawing.Color.Azure;
+            this.bPlus.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bPlus.Location = new System.Drawing.Point(166, 294);
+            this.bPlus.Name = "bPlus";
+            this.bPlus.Size = new System.Drawing.Size(45, 39);
+            this.bPlus.TabIndex = 22;
+            this.bPlus.Text = "+";
+            this.bPlus.UseVisualStyleBackColor = false;
+            this.bPlus.Click += new System.EventHandler(this.bPlus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(280, 347);
+            this.Controls.Add(this.bPlus);
+            this.Controls.Add(this.bPunct);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.bEgal);
+            this.Controls.Add(this.bMinus);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.bInvers);
+            this.Controls.Add(this.bOri);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.bMod);
+            this.Controls.Add(this.bDiv);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.bRadical);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.bCE);
+            this.Controls.Add(this.bC);
+            this.Controls.Add(this.bSageata);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bSageata;
+        private System.Windows.Forms.Button bC;
+        private System.Windows.Forms.Button bCE;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button bRadical;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button bDiv;
+        private System.Windows.Forms.Button bMod;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button bOri;
+        private System.Windows.Forms.Button bInvers;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button bMinus;
+        private System.Windows.Forms.Button bEgal;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bPunct;
+        private System.Windows.Forms.Button bPlus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void bPunct_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ",";
+        }
+
+        private void bSageata_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length > 0)
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+        }
+
+        private void bDiv_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "/";
+        }
+
+        private void bOri_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "*";
+        }
+
+        private void bMinus_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "-";
+        }
+
+        private void bPlus_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+";
+        }
+
+        private void bMod_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string[] op = s.Split('+', '-', '*', '/', '%');
+            op[2].Remove(op[2].Length - 1);
+            double nr1 = Convert.ToDouble(op[1]);
+            double nr2 = Convert.ToDouble(op[2]);
+            int p = 0;
+            int p1 = s.IndexOf('-'); if (p1 != -1) p = p1;
+            int p2 = s.IndexOf('+'); if (p2 != -1) p = p2;
+            int p3 = s.IndexOf('*'); if (p3 != -1) p = p3;
+            int p4 = s.IndexOf('/'); if (p4 != -1) p = p4;
+            s = s.Remove(p + 1);
+            textBox1.Text +=  Convert.ToString(nr2 / 100 * nr1);
+        }
+
+        private void bRadical_Click(object sender, EventArgs e)
+        {
+            double nr = Convert.ToDouble(textBox1.Text);
+            if (nr >= 0)
+                textBox1.Text = Convert.ToString(Math.Sqrt(nr));
+            else
+                textBox1.Text = "Eroare";
+        }
+
+        private void bInvers_Click(object sender, EventArgs e)
+        {
+            double nr = Convert.ToDouble(textBox1.Text);
+            if(nr != 0)
+                textBox1.Text = Convert.ToString(1 / nr);
+            else
+                textBox1.Text = "Eroare";
+        }
+
+
+        private void bCE_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            double nr1 = 1, nr2 = 1;
+            if (s.IndexOf('-') == 0)
+            {
+                nr1 = -1;
+                s = s.Remove(0, 1);
+            }
+            int poz = s.IndexOf('-');
+            if (poz != -1 && (!(s[poz - 1] >= '0' && s[poz - 1] <= '9') || s[poz + 1] == '-'))
+            {
+                nr2 = -1;
+                s = s.Remove(poz, 1);
+            }
+            int p = 0;
+            int p1 = s.IndexOf('-'); if (p1 != -1) p = p1;
+            int p2 = s.IndexOf('+'); if (p2 != -1) p = p2;
+            int p3 = s.IndexOf('*'); if (p3 != -1) p = p3;
+            int p4 = s.IndexOf('/'); if (p4 != -1) p = p4;
+
+            s = s.Remove(p + 1);
+            if (nr1 == -1)
+                s = "-" + s;
+            textBox1.Text = s;
+        }
+
+        private void bC_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void bEgal_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            double nr1 = 1, nr2 = 1;
+            if(s.IndexOf('-') == 0)
+            {
+                nr1 = -1;
+                s = s.Remove(0, 1);
+            }
+            int poz = s.IndexOf('-');
+            if(poz != -1 && (!(s[poz - 1] >= '0' && s[poz - 1] <='9') || s[poz + 1] == '-'))
+            {
+                nr2 = -1;
+                s = s.Remove(poz, 1);
+            }
+            string[] op = s.Split('+', '-', '*', '/', '%');
+            nr1 = nr1 * Convert.ToDouble(op[0]);
+            nr2 = nr2 * Convert.ToDouble(op[1]);
+            if (s.IndexOf('+') != -1) textBox1.Text = Convert.ToString(nr1 + nr2);
+            else
+                if (s.IndexOf('-') != -1) textBox1.Text = Convert.ToString(nr1 - nr2);
+                else
+                    if (s.IndexOf('*') != -1) textBox1.Text = Convert.ToString(nr1 * nr2);
+                    else
+                        if (s.IndexOf('/') != -1) textBox1.Text = Convert.ToString(nr1 / nr2);
+                        else textBox1.Text = "Eroare!";
+                }
+    }
+}
